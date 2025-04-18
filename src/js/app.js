5043aa82ea3836c34b5d3780d60145f008a3330b
pauchok-sk <pauchoooooook@gmail.com>
2025-04-18 15:27:36 +0300

    footer

--- src/js/app.js
+++ src/js/app.js
@@ -2,6 +2,7 @@ import "../scss/style.scss";
 import burger from "./files/burger.js";
 import inputmask from "./files/inputmask.js";
 import map from "./files/map.js";
+import mediaAdaptive from "./files/mediaAdaptive.js";
 import sliders from "./files/sliders.js";
 import spoller from "./files/spoller.js";
 
@@ -10,5 +11,9 @@ burger();
 map();
 sliders();
 inputmask();
+mediaAdaptive();
 
-Fancybox.bind("[data-fancybox]")
+Fancybox.bind("[data-fancybox]", {
+  closeButton: false,
+});
+// Fancybox.show([{src: "#modal-connect", type: "inline"}])
